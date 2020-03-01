e234024cf0f542cba831c509b4afadc94a88b197
DerekPeacock <derek.peacock@yahoo.com>
2020-03-01 10:46:12 +0000

    Added the calculator and the splash screen

---
+++ DerekWindowsFormsApp/U1_CalculatorForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DerekWindowsFormsApp
+{
+    public partial class U1_CalculatorForm : Form
+    {
+        public U1_CalculatorForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
