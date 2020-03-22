d681473a30a132def42ea220f52d9841efc05e9e
DerekPeacock <derek.peacock@yahoo.com>
2020-03-22 20:07:15 +0000

    Have added the two tipster forms for 4.1 to 4.6

---
+++ DerekWindowsFormsApp/Unit4/TipsterForm.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace DerekWindowsFormsApp.Unit4
+{
+    partial class TipsterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TipsterForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.billTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.quitButton = new System.Windows.Forms.Button();
+            this.calculateButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(358, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(278, 321);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bill (£)";
+            // 
+            // billTextBox
+            // 
+            this.billTextBox.Location = new System.Drawing.Point(151, 46);
+            this.billTextBox.Name = "billTextBox";
+            this.billTextBox.Size = new System.Drawing.Size(100, 32);
+            this.billTextBox.TabIndex = 2;
+            this.billTextBox.Text = "120.00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "People";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(151, 125);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(100, 32);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // quitButton
+            // 
+            this.quitButton.Location = new System.Drawing.Point(525, 392);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(111, 41);
+            this.quitButton.TabIndex = 5;
+            this.quitButton.Text = "Quit";
+            this.quitButton.UseVisualStyleBackColor = true;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(358, 392);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(136, 41);
+            this.calculateButton.TabIndex = 6;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            // 
+            // TipsterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 445);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.quitButton);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.billTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "TipsterForm";
+            this.Text = "Derek\'s Tipster Bill Entry";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox billTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button quitButton;
+        private System.Windows.Forms.Button calculateButton;
+    }
+}
---
+++ DerekWindowsFormsApp/Unit4/TipsterResults.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace DerekWindowsFormsApp.Unit4
+{
+    partial class TipsterResults
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.billTextBox = new System.Windows.Forms.TextBox();
+            this.peopleTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.groupBox1.Controls.Add(this.peopleTextBox);
+            this.groupBox1.Controls.Add(this.billTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.ForeColor = System.Drawing.Color.Red;
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(353, 129);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bill Details";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(27, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Total Bill";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(27, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "People";
+            // 
+            // billTextBox
+            // 
+            this.billTextBox.BackColor = System.Drawing.Color.LightGray;
+            this.billTextBox.Enabled = false;
+            this.billTextBox.Location = new System.Drawing.Point(173, 38);
+            this.billTextBox.Name = "billTextBox";
+            this.billTextBox.ReadOnly = true;
+            this.billTextBox.Size = new System.Drawing.Size(100, 32);
+            this.billTextBox.TabIndex = 2;
+            // 
+            // peopleTextBox
+            // 
+            this.peopleTextBox.BackColor = System.Drawing.Color.LightGray;
+            this.peopleTextBox.Enabled = false;
+            this.peopleTextBox.Location = new System.Drawing.Point(173, 80);
+            this.peopleTextBox.Name = "peopleTextBox";
+            this.peopleTextBox.ReadOnly = true;
+            this.peopleTextBox.Size = new System.Drawing.Size(100, 32);
+            this.peopleTextBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 26);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Each Pays:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightGray;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(186, 197);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 32);
+            this.textBox1.TabIndex = 4;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(272, 250);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(94, 40);
+            this.closeButton.TabIndex = 5;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            // 
+            // TipsterResults
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 302);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "TipsterResults";
+            this.Text = "Derek\'s Tipster Results";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox peopleTextBox;
+        private System.Windows.Forms.TextBox billTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button closeButton;
+    }
+}
